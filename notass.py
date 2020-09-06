225394b48ed5ab65eabc55865814c7918d2a1b76
Alonso R <alonso.vladimir.94@gmail.com>
2020-09-06 18:12:18 -0400

    notas.1.4

--- notass.py
+++ notass.py
@@ -1,4 +1,4 @@
-notas={'nombre':('Aquiles','Elba','Alan','Armando'),'apellidos':('Baeza','Zurita','Brito','Casas'),'progra':(6.4,5.6,4.5,5.4),'fis':(5.9,6.3,4.3,7.0)}
+notas={'nombre':('Aquiles','Elba','Alan','Armando'),'apellido':('Baeza','Zurita','Brito','Casas'),'progra':(6.4,5.6,4.5,5.4),'fis':(5.9,6.3,4.3,7.0)}
 print(notas['nombre'][0])
 print(notas['apellido'][0])
 print(notas['progra'][0])
