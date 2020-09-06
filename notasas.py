507d22bd4562e8f8ac82be760a81875df6fefa58
Alonso R <alonso.vladimir.94@gmail.com>
2020-09-06 18:36:33 -0400

    notas1.x

---
+++ notasas.py
@@ -0,0 +1,6 @@
+notas={1:('Nombre','Aquiles','Elba','Alan','Armando'),2:('Apellido','Baeza','Zurita','Brito','Casas'),3:('Programación',5.6,5.8,7.0,6.4),4:('Física',4.5,5.4,7.0,4.6)}
+v=(1,2,3,4)
+for i in v:
+	print(notas[i][0]);print(notas[i][2])
+
+
